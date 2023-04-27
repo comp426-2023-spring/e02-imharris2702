d1057883df467bf1ab52d9e3cb2dc51ebd9eccdf
Isaiah Harris <69725417+imharris2702@users.noreply.github.com>
2023-04-27 17:23:12 -0400

    Define reset

--- public/scripts.js
+++ public/scripts.js
@@ -2,13 +2,14 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 function showHideShots() {
-
+    
 }
 
 function reset() {
-
+    document.getElementById('game_settings').reset();
+    showHideShots();
 }
 
 function playGame() {
-    
+
 }
